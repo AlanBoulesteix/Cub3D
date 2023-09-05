da5ee9bc509308cc1ef16395aa3c9cbe43434d75
Charlotte MADRANGEAS <charlotte.madrangeas@gmail.com>
2023-09-05 14:53:04 +0200

    handle double separators / error in rgb separators overall

--- src/rgb_utils.c
+++ src/rgb_utils.c
@@ -3,28 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   rgb_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
+/*   By: chmadran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 13:47:24 by aboulest          #+#    #+#             */
-/*   Updated: 2023/09/05 13:51:14 by aboulest         ###   ########.fr       */
+/*   Updated: 2023/09/05 14:52:03 by chmadran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	count_sep(char *line)
+int	valid_sep(char *line)
 {
 	int		i;
 	int		count;
 
 	i = 0;
 	count = 0;
-	while (line[++i])
+	while (line[i] && !ft_isdigit(line[i]))
 	{
 		if (line[i] == ',')
 			count++;
+		i++;
 	}
-	return (count);
+	return (count != 1);
+}
+
+int	count_nb_int(char *line)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (line[i])
+	{
+		if (ft_isdigit(line[i]))
+		{
+			count++;
+			while (line[i] && ft_isdigit(line[i]))
+				i++;
+			if (line[i] && count < 3 && valid_sep(&line[i]) == EXIT_FAILURE)
+				return (EXIT_FAILURE);
+		}
+		if (!line[i] && count == 3)
+			return (EXIT_SUCCESS);
+		else if (!line[i] && count != 3)
+			return (EXIT_FAILURE);
+		i++;
+	}
+	if (count > 4)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
 
 bool	rgb_is_valid(char *line)
@@ -39,7 +68,7 @@ bool	rgb_is_valid(char *line)
 			return (false);
 		i++;
 	}
-	if (count_sep(line) != 2)
+	if (count_nb_int(line) == EXIT_FAILURE)
 		return (false);
 	return (true);
 }
